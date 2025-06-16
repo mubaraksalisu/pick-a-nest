a532a714f07bb5acb6ffdf615da8b7fe6ea5b591
mubarak <marksmanl619@gmail.com>
2025-06-16 19:24:33 +0100

    created empty resources for all known endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FavoritesModule } from './favorites/favorites.module';
+import { UsersModule } from './users/users.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { StatesModule } from './states/states.module';
+import { CategoriesModule } from './categories/categories.module';
+import { PropertiesModule } from './properties/properties.module';
+import { AuthModule } from './auth/auth.module';
+import { VisitsModule } from './visits/visits.module';
 
 @Module({
-  imports: [],
+  imports: [FavoritesModule, UsersModule, ReviewsModule, StatesModule, CategoriesModule, PropertiesModule, AuthModule, VisitsModule],
   controllers: [AppController],
   providers: [AppService],
 })
