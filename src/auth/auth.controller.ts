defe8a7956412c30536ea24e0c1e7bf6001c580b
Mubarak <marksmanl619@gmail.com>
2025-12-27 12:45:31 +0100

    Feat(auth): Document the auth endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,26 +13,31 @@ import { Request } from 'express';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { Throttle } from '@nestjs/throttler';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthPayloadDto } from './dto/auth.dto';
 
 @Throttle({ default: { limit: 10, ttl: 60000 } })
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalGuard)
   @Post('login')
+  @ApiResponse({ status: 201, description: 'user login success', type: String })
   login(@Req() req: Request, @Body(ValidationPipe) authDto: AuthPayloadDto) {
     return this.authService.login(req.user);
   }
 
   @Post('register')
+  @ApiResponse({ status: 201, description: 'Create a new user' })
   register(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
+  @ApiResponse({ status: 200, description: 'Return user profile' })
   profile(@Req() req: Request) {
     return req.user;
   }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,13 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class AuthPayloadDto {
   @IsEmail()
   @MinLength(5)
   @MaxLength(255)
+  @ApiProperty({
+    example: 'mail@email.com',
+    description: 'email address of the user',
+  })
   email: string;
 
   @IsString()
   @MinLength(5)
   @MaxLength(255)
+  @ApiProperty({ example: '12345', description: 'password of the user' })
   password: string;
 }
