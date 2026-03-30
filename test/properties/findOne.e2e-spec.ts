7ff825c8c7669c41da082b53d75c6f4016424c3a
mubarak salisu <marksmanl619@gmail.com>
2026-03-30 11:47:27 +0100

    Test-E2E(properties.service.findOne): write test for the findOne method of the properties service

--- test/properties/findOne.e2e-spec.ts
+++ test/properties/findOne.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { getConnectionToken, getModelToken } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
-import { Connection, Model } from 'mongoose';
+import { Connection, Model, Types } from 'mongoose';
 import { AppModule } from 'src/app.module';
 import { Category } from 'src/modules/categories/schemas/category.schema';
 import { Property } from 'src/modules/properties/schemas/property.schema';
 import { User } from 'src/modules/users/schemas/user.schema';
 import * as request from 'supertest';
 
-describe('PropertiesService.findAll - E2E', () => {
+describe('PropertiesService.findOne - E2E', () => {
   let app: INestApplication;
   let dbConnection: Connection;
   let userModel: Model<User>;
@@ -16,7 +16,7 @@ describe('PropertiesService.findAll - E2E', () => {
   let propertyModel: Model<Property>;
   let testUser: any;
   let testCategory: any;
-  let testProperties: any[] = [];
+  let testProperty: any;
 
   let propertyDto = {
     title: 'duplex',
@@ -74,15 +74,11 @@ describe('PropertiesService.findAll - E2E', () => {
 
     testUser = await userModel.create(userDto);
     testCategory = await categoryModel.create(categoryDto);
-    for (let i = 0; i < 15; i++) {
-      const property = await propertyModel.create({
-        ...propertyDto,
-        title: `Test Property ${i + 1}`,
-        ownerId: testUser._id,
-        categoryId: testCategory._id,
-      });
-      testProperties.push(property);
-    }
+    testProperty = await propertyModel.create({
+      ...propertyDto,
+      ownerId: testUser._id,
+      categoryId: testCategory._id,
+    });
   });
 
   afterAll(async () => {
@@ -90,18 +86,38 @@ describe('PropertiesService.findAll - E2E', () => {
     await app.close();
   });
 
-  it('should return paginated properties', async () => {
+  it('should return NotFoundException if no property with the provided id exists', async () => {
+    const nonExistentId = new Types.ObjectId().toString();
     const response = await request(app.getHttpServer())
-      .get('/properties')
-      .query({ page: 1, limit: 10 })
+      .get(`/properties/${nonExistentId}`)
+      .expect(404);
+
+    expect(response.body.message).toBe('No property with the provided id');
+  });
+
+  it('should return the property if it exists', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/properties/${testProperty._id}`)
       .expect(200);
 
-    expect(response.body).toHaveProperty('data');
-    expect(Array.isArray(response.body.data)).toBe(true);
-    expect(response.body.data.length).toBeLessThanOrEqual(10);
-    expect(response.body.total).toBe(15);
-    expect(response.body.page).toBe('1');
-    expect(response.body.limit).toBe('10');
-    expect(response.body.totalPage).toBe(2);
+    expect(response.body._id).toBe(testProperty._id.toString());
+    expect(response.body.title).toBe(propertyDto.title);
+    expect(response.body.description).toBe(propertyDto.description);
+    expect(response.body.address).toBe(propertyDto.address);
+    expect(response.body.city).toBe(propertyDto.city);
+    expect(response.body.state).toBe(propertyDto.state);
+    expect(response.body.price).toBe(propertyDto.price);
+    expect(response.body.transactionType).toBe(propertyDto.transactionType);
+    expect(response.body.bedroom).toBe(propertyDto.bedroom);
+    expect(response.body.bathroom).toBe(propertyDto.bathroom);
+    expect(response.body.livingRoom).toBe(propertyDto.livingRoom);
+    expect(response.body.parkingSpace).toBe(propertyDto.parkingSpace);
+    expect(response.body.pool).toBe(propertyDto.pool);
+    expect(response.body.ownerId._id).toBe(testUser._id.toString());
+    expect(response.body.categoryId._id).toBe(testCategory._id.toString());
+    expect(response.body.status).toBe(propertyDto.status);
+    expect(response.body.media).toEqual(propertyDto.media);
+    expect(response.body.rentDuration).toBe(propertyDto.rentDuration);
+    expect(response.body.propertySize).toBe(propertyDto.propertySize);
   });
 });
